a601c4c2bd07f81f36dd4c52da391b7517d36394
Yu0972 <105021045@live.asia.edu.tw>
2016-10-31 16:09:58 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,18 +1,32 @@
 ﻿package ex;
+import java.util.Scanner;
+
 /*
  * Topic: 寫一函數 square(n,c)，以 c 字元，印出邊長為 n 之實心正方形。Ex：square(3,'@') 將印出如下形狀
  @@@
  @@@
  @@@
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021045 郭毓呈
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner (System.in) ;
+		System.out.print("輸入 n =");
+		int n = scn.nextInt () ;
+		System.out.print("輸入 c =");
+		char c = scn.next().charAt(0);
+		fun1(n, c);
 	}
-
+		public static void fun1( int n , char c ) {
+			for( int a = 1 ; a <= n ; a ++ ){
+			for( int b = 1 ; b <= n ; b ++ ){
+				System.out.print(c);
+			}
+			System.out.println();
+			}
+	}
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,31 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021045 郭毓呈
  */
 
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner (System.in) ;
+		System.out.print("輸入 m =");
+		long m = scn.nextInt () ;
+		System.out.print("輸入 n =");
+		long n = scn.nextInt () ;
+		fun1(m, n);
 	}
-
+	public static void fun1( long m , long n ) {
+		long t = 1 ;
+		long u = 1 ;
+		for ( long a = m ; a > 0 ; a -- ) {
+			t = t * a ;
+		for ( long b = n ; b > 0 ; b -- ) {
+			u = u * b ;
+	}
+}
+		System.out.println(t/u);
+	}
 }
