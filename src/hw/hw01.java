e6ca8dfc9fc598de2a6a90f3cbe8b3e0e12e3b8d
Yu0972 <105021045@live.asia.edu.tw>
2016-11-04 09:21:42 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,23 @@
 package hw;
-
+import java.util.Scanner ;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner ( System.in ) ;
+	    	System.out.print ( "輸入溫度=" ) ;
+	    	float a = scn.nextFloat ( ) ;
+	    	System.out.print ( "輸入1計算華氏為攝氏、輸入2計算攝氏為華氏=" ) ;
+	    	float b = scn.nextFloat ( ) ;
+	    	System.out.println ( fun (  a , b ) ) ;
+}
+	public static float fun ( float a , float b ) {
+			float c = 0 ;
+			if( b == 1 ) {
+			c = ( a - 32 ) *  5 / 9  ;
+		}else{
+			c =  a * 9 / 5 + 32 ;
+}
+			return c ;
 	}
-
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,22 @@
 package hw;
-
+import java.util.Scanner ;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner ( System.in ) ;
+		System.out.print ( "依序輸入性別、與身高=" ) ;
+		System.out.print ( fun ( scn.nextInt( ) , scn.nextFloat ( ) ) ) ;
+}
+	public static float fun ( int a , float b ) {
+		float c = 0 ;
+		if ( a == 1 ) {
+			c = ( float ) ( ( b - 170 ) * 0.6 + 62 ) ;
+	} else {
+		if ( a == 2 ) {
+			c = ( float ) ( ( b - 158 ) * 0.5 + 52 ) ;
+}
 	}
-
+		return c ;
+	}
 }
